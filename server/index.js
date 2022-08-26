9f686f5af44b7ad5b67d77a569120c20360f937a
jeroen nepveuE <jnepveu88@gmail.com>
2022-08-26 17:47:14 +0200

    nodemon added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
+// ---------------------requirements----------------//
 const express = require("express");
 const compression = require("compression");
-const PORT = process.env.PORT || 3001;
 const app = express();
 const db = require("./database/db");
+const PORT = process.env.PORT || 3001;
+// ---------------------middleware----------------//
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -10,20 +12,21 @@ app.use(compression());
 
 app.use(express.json());
 
+// ---------------------routing----------------//
+
 app.post("/api/newuser", async (req, res) => {
     console.log("POST /registration");
     const { userName } = req.body;
     try {
         const { rows } = await db.newUser(userName);
-        res.json(rows[0]); //username
+        res.json(rows[0]);
     } catch (err) {
         console.log("err: ", err);
         res.json({ success: false });
     }
 });
 
+// ---------------------server----------------//
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-//THIS IS DATABASE: psql -d scoringdatabase
